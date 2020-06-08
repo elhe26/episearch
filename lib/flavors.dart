d530f442d631bb590ff3505272561779a66a59a9
Erick Heredia <erick.heredia@gmail.com>
2020-06-07 23:57:22 -0400

    Agregando soporte para flavors.

---
+++ lib/flavors.dart
@@ -0,0 +1,20 @@
+enum Flavor {
+  PRODUCTION,
+  DEVELOPMENT,
+}
+
+class F {
+  static Flavor appFlavor;
+
+  static String get title {
+    switch (appFlavor) {
+      case Flavor.PRODUCTION:
+        return 'EpiSearch';
+      case Flavor.DEVELOPMENT:
+        return 'EpiSearch Dev';
+      default:
+        return 'title';
+    }
+  }
+
+}
