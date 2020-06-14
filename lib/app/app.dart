8897745ab7ab78a44ad89f6a3ac21c6d68c24a90
Erick Heredia <erick.heredia@gmail.com>
2020-06-14 17:32:13 -0400

    Modificando EpiSearchApp #2
    Agregando configuracion inicial.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,25 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import 'locator.dart';
+import 'routes.gr.dart';
 
 class EpiSearchApp extends StatelessWidget {
+  final Color primaryColor;
+
+  const EpiSearchApp({Key key, this.primaryColor}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "EpiSearch",
+      navigatorKey: locator<NavigationService>().navigatorKey,
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.of(context).locale,
+      initialRoute: Routes.splashViewRoute,
+      onGenerateRoute: Router().onGenerateRoute,
+      theme: ThemeData(primaryColor: primaryColor),
+    );
   }
 }
