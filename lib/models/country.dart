a8be85531d60b338b886b7dace03c40915545d7d
Erick Heredia <erick.heredia@gmail.com>
2020-06-15 17:27:50 -0400

    Creacion modelos iniciales.

---
+++ lib/models/country.dart
@@ -0,0 +1,40 @@
+class Country {
+  final String country;
+  final String countryCode;
+  final String slug;
+  final int newConfirmed;
+  final int totalConfirmed;
+  final int newDeaths;
+  final int totalDeaths;
+  final int newRecovered;
+  final int totalRecovered;
+  final String date;
+
+  Country({
+    this.country,
+    this.countryCode,
+    this.slug,
+    this.newConfirmed,
+    this.totalConfirmed,
+    this.newDeaths,
+    this.totalDeaths,
+    this.newRecovered,
+    this.totalRecovered,
+    this.date,
+  });
+
+  factory Country.fromJson(Map<String, dynamic> json) {
+    return Country(
+      country: json["Country"] as String,
+      countryCode: json["CountryCode"] as String,
+      slug: json["Slug"] as String,
+      newConfirmed: json["NewConfirmed"] as int,
+      totalConfirmed: json["TotalConfirmed"] as int,
+      newDeaths: json["NewDeaths"] as int,
+      totalDeaths: json["TotalDeaths"] as int,
+      newRecovered: json["NewRecovered"] as int,
+      totalRecovered: json["TotalRecovered"] as int,
+      date: json["Date"] as String,
+    );
+  }
+}
