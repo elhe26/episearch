b8032167103bd98860e9d9e383d49a564aa41335
Erick Heredia <erick.heredia@gmail.com>
2020-06-15 22:21:06 -0400

    Soporte para animaciones splash.

--- lib/ui/views/splash/splash_view.dart
+++ lib/ui/views/splash/splash_view.dart
@@ -1,16 +1,29 @@
-
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 import 'splash_viewmodel.dart';
+import '../../shared/app_colors.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<SplashViewModel>.reactive(
-      builder: (context, model, child) => Scaffold(),
+    return ViewModelBuilder<SplashViewModel>.nonReactive(
+      builder: (context, model, child) => SplashScreen.callback(
+        name: model.logo,
+        until: () {
+          model.getAPIData();
+          return Future.delayed(Duration(seconds: 5));
+        },
+        onSuccess: (_) => model.navigateToHome(),
+        onError: (error, stacktrace) {},
+        startAnimation: "intro",
+        loopAnimation: "intro",
+        endAnimation: "intro",
+        backgroundColor: lightMonoGrey,
+      ),
       viewModelBuilder: () => SplashViewModel(),
     );
   }
