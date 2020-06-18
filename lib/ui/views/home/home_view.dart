bde3528e0eb5f243bd6173e3cdd620d39d6ed9c7
Erick Heredia <erick.heredia@gmail.com>
2020-06-18 18:52:38 -0400

    Modificando home view.
    Convirtiendo pasando viewmodel a  HomeView usando ViewModelWidet con el viemodel global.

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
 
-import 'home_viewmodel.dart';
 import '../../shared/app_colors.dart';
 import '../../shared/shared_styles.dart';
 import '../../widgets/home_appbar.dart';
@@ -10,58 +9,53 @@ import '../../widgets/test_card.dart';
 import '../../widgets/global_card.dart';
 import '../../widgets/local_card.dart';
 import '../../widgets/notes_carousel.dart';
+import '../../../app/app_viewmodel.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends ViewModelWidget<EpiSearchViewModel> {
   const HomeView({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ViewModelBuilder<HomeViewModel>.reactive(
-      builder: (context, model, child) {
-        return Scaffold(
-          backgroundColor: whiteMonoLetter,
-          appBar: HomeBar(),
-          body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  SizedBox(height: smallFieldHeight),
-                  GestureDetector(
-                    child: TestCard(),
-                    onTap: () {},
-                  ),
-                  SizedBox(height: smallFieldHeight),
-                  GestureDetector(
-                    child: GlobalCard(),
-                    onTap: () {},
-                  ),
-                  SizedBox(height: smallFieldHeight),
-                  GestureDetector(
-                    child: LocalCard(),
-                    onTap: () {},
-                  ),
-                  SizedBox(height: smallFieldHeight),
-                  Text(
-                    "Notas Importantes",
-                    style: GoogleFonts.quicksand(
-                      color: blackMonoLetter,
-                      fontSize: 22,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  SizedBox(height: inputFieldBottomMargin),
-                  NotesCarousel(),
-                ],
+  Widget build(BuildContext context, EpiSearchViewModel model) {
+    return Scaffold(
+      appBar: HomeBar(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SizedBox(height: smallFieldHeight),
+              GestureDetector(
+                child: TestCard(),
+                onTap: () {},
               ),
-            ),
+              SizedBox(height: smallFieldHeight),
+              GestureDetector(
+                child: GlobalCard(),
+                onTap: () {},
+              ),
+              SizedBox(height: smallFieldHeight),
+              GestureDetector(
+                child: LocalCard(),
+                onTap: () {},
+              ),
+              SizedBox(height: smallFieldHeight),
+              Text(
+                "Notas Importantes",
+                style: GoogleFonts.quicksand(
+                  color: model.isDarkTheme ? whiteMonoLetter : blackMonoLetter,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: inputFieldBottomMargin),
+              NotesCarousel(),
+            ],
           ),
-        );
-      },
-      viewModelBuilder: () => HomeViewModel(),
+        ),
+      ),
     );
   }
 }
