415d6665290730f8e925f344382aeba699b6882d
Erick Heredia <erick.heredia@gmail.com>
2020-06-18 18:47:51 -0400

    Agregando servicio Data Cache.

--- lib/services/data_cache_service.dart
+++ lib/services/data_cache_service.dart
@@ -1,4 +1,25 @@
 import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-@injectable
-class DataCacheService {}
+@lazySingleton
+class DataCacheService {
+  static const THEME_STATUS = "THEMESTATUS";
+  bool _cacheDarkTheme = false;
+
+  // Getters
+  bool cacheDarkTheme() {
+    return _cacheDarkTheme;
+  }
+
+  // Services
+  Future<void> setTheme({bool value}) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setBool(THEME_STATUS, value);
+    _cacheDarkTheme = value;
+  }
+
+  Future<void> getTheme() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    _cacheDarkTheme = preferences.getBool(THEME_STATUS) ?? false;
+  }
+}
