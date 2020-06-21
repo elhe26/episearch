dc43ffafe42a5e5a910ae86873f6a312e1517557
Erick Heredia <erick.heredia@gmail.com>
2020-06-21 03:40:40 -0400

    Modificacion viewmodel splash.

--- lib/ui/views/splash/splash_viewmodel.dart
+++ lib/ui/views/splash/splash_viewmodel.dart
@@ -3,12 +3,14 @@ import 'package:stacked_services/stacked_services.dart';
 
 import '../../../app/locator.dart';
 import '../../../app/routes.gr.dart';
+import '../../../services/api_service.dart';
 import '../../../services/data_cache_service.dart';
 
 class SplashViewModel extends BaseViewModel {
   // * Services
-  NavigationService _navigationService = locator<NavigationService>();
-  DataCacheService _dataCacheService = locator<DataCacheService>();
+  final _navigationService = locator<NavigationService>();
+  final _dataCacheService = locator<DataCacheService>();
+  final _apiService = locator<APIService>();
 
   // * Variables
   String _logo = "assets/logo/EpiSearch.flr";
@@ -22,5 +24,46 @@ class SplashViewModel extends BaseViewModel {
   // * Business Logic
   Future navigateToHome() async {
     await _navigationService.replaceWith(Routes.homeViewRoute);
+  }
+
+  void initialize() async {
+    final global = _dataCacheService.getGlobalData;
+    final countries = _dataCacheService.getCountriesData;
+
+    final summaryResponse = await getSummaryData();
+
+    if (!summaryResponse) {
+      if (global == null || countries == null) {
+      } else {
+        _apiService.global = global;
+        _apiService.countries = countries;
+      }
+    }
+    notifyListeners();
+  }
+
+  Future getSummaryData() async {
+    setBusy(true);
+
+    final response = await _apiService.summary();
+
+    setBusy(false);
+
+    if (response is bool) {
+      if (response) {
+        final globalValue = _apiService.global;
+        final countriesValue = _apiService.countries;
+        _dataCacheService.setCacheData(
+          global: globalValue,
+          countries: countriesValue,
+        );
+        notifyListeners();
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
   }
 }
